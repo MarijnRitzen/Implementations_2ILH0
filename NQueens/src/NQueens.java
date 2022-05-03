4f14177244c65cba4d61fbcdcf52faac7965b072
Marijn <Marijnritzen@outlook.com>
2022-05-03 18:08:06 +0200

    First implementation 1.1

--- NQueens/src/NQueens.java
+++ NQueens/src/NQueens.java
@@ -16,8 +16,36 @@ public class NQueens {
 
 	// perform best iterative improvement (solution should be changed directly)
 	public static void bestIterativeImprovement(NQueenInstance inst, NQBasicSol sol) {
-		
-		// TODO
+
+		while (true) {
+			int lowestCost = sol.getCost();
+			int bestMoveQueen = -1;
+			int bestMove = -1;
+
+			for (int queen = 0; queen < inst.N; queen++) { // For every queen
+				for (int move = 0; move < 8; move++) { // For every move
+					if (sol.applyLocalMove(queen, move)) {
+						// If applyLocalMove returns True, move was valid and
+						// we check new cost and have to inverse the move
+						int currentCost = sol.getCost();
+						if (currentCost < lowestCost) {
+							lowestCost = currentCost;
+							bestMoveQueen = queen;
+							bestMove = move;
+						}
+
+						sol.undoLocalMove(queen, move);
+					}
+				}
+			}
+
+			// If we have reached a local minimum, the best improvement is as good as the current solution and thus
+			// we never got into the if statement that modifies bestMove, so we quit the while loop
+			if (bestMove == -1)
+				break;
+
+			sol.applyLocalMove(bestMoveQueen, bestMove);
+		}
 
 	}
 	
