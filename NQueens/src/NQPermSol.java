7f1faa0d25b04894b65f71a81ac0c97cfd31bc9b
Marijn <Marijnritzen@outlook.com>
2022-05-17 16:20:51 +0200

    Queens question yet to be committed

--- NQueens/src/NQPermSol.java
+++ NQueens/src/NQPermSol.java
@@ -1,26 +1,70 @@
+import java.util.Random;
 
 public class NQPermSol extends NQueenSol {
 
-	// TODO: needs a solution representation
+	// Solution representation. Looking at the columns from left to right,
+	// this array holds in order the row coordinates of the queens.
+	// Of course this presumes the queens all have their own columns, but
+	// we will make sure of that in the instantiation.
+	int[] rowPositions;
 
-	// standard constructor
+
 	public NQPermSol(NQueenInstance inst, boolean random) {
 		super(inst);
 
-		// TODO: fill in the rest of the constructor
+		// Initialize to -1 (needed when we want randomization).
+		rowPositions = new int[inst.N];
+		for (int col = 0; col < inst.N; col++) {
+			rowPositions[col] = -1;
+		}
+
+		if (!random) {
+			// Place all queens on the diagonal
+			for (int i = 0; i < N; i++) {
+				rowPositions[i] = i;
+			}
+		}
+		else {
+			// Place queens randomly (we allow two queens on the same position)
+			Random rand = new Random();
+			for (int i = 0; i < N; i++) {
+				int rowPosition = rand.nextInt(N);
+				while (rowTaken(rowPosition)) {
+					rowPosition = rand.nextInt(N);
+				}
+				rowPositions[i] = rowPosition;
+			}
+		}
 	}
-	
-	
-	
+
+	private boolean rowTaken(int row) {
+		for (int i = 0; i < rowPositions.length; i++) {
+			if (rowPositions[i] == row) return true;
+		}
+
+		return false;
+	}
+
 	// get the position of a queen
-	public Pos getPosition(int i) {
-		
-		// TODO: must be filled in! Should return position of i^th queen
-		
-		return null;
+	public Pos getPosition(int i) { return new Pos(i, rowPositions[i]);	}
+
+	// Swaps the columns of two queens indicated by their column positions firstQueenColumn and secondQueenColumn.
+	// If both queen columns are the same, return false. Otherwise return true.
+	public boolean swapQueenColumns(int firstQueenColumn, int secondQueenColumn) {
+		if (firstQueenColumn == secondQueenColumn) return false;
+
+		int temp = rowPositions[firstQueenColumn];
+		rowPositions[firstQueenColumn] = rowPositions[secondQueenColumn];
+		rowPositions[secondQueenColumn] = temp;
+
+		return true;
 	}
-	
-	
-	// TODO: Don't forget to implement a local move (along with the undo)!
+
+	// Undo a local move (must be executed after performing the same local move for correct behavior)
+	public void undoLocalSwap(int firstQueenColumn, int secondQueenColumn) {
+		int temp = rowPositions[firstQueenColumn];
+		rowPositions[firstQueenColumn] = rowPositions[secondQueenColumn];
+		rowPositions[secondQueenColumn] = temp;
+	}
 	
 }
--- NQueens/src/NQueens.java
+++ NQueens/src/NQueens.java
@@ -4,9 +4,9 @@ public class NQueens {
 	public static void main(String[] args) {
 		String dataset = "8"; // choose the dataset
 		NQueenInstance inst = new NQueenInstance("data/" + dataset + ".txt"); // load the problem instance
-		NQBasicSol sol = new NQBasicSol(inst, true); // initialize a (random) solution
-		//NQPermSol sol = new NQPermSol(inst, true); // initialize a (random) solution
-		bestIterativeImprovement(inst, sol);
+//		NQBasicSol sol = new NQBasicSol(inst, true); // initialize a (random) solution
+		NQPermSol sol = new NQPermSol(inst, true); // initialize a (random) solution
+		bestIterativeImprovement2(inst, sol);
 		System.out.println("Cost = " + sol.getCost());
 		sol.visualize("figures/" + dataset + ".png");
 		sol.output("output/" + dataset + ".txt");
@@ -52,10 +52,35 @@ public class NQueens {
 	
 
 	// perform best iterative improvement (solution should be changed directly)
-	public static void bestIterativeImprovement(NQueenInstance inst, NQPermSol sol) {
-		
-		// TODO
-		
+	public static void bestIterativeImprovement2(NQueenInstance inst, NQPermSol sol) {
+		while (true) {
+			int lowestCost = sol.getCost();
+			int[] bestSwap = new int[]{-1, -1};
+
+			for (int queenOne = 0; queenOne < inst.N; queenOne++) { // For every queen
+				for (int queenTwo = queenOne + 1; queenTwo < inst.N; queenTwo++) { // For every other queen
+					if (sol.swapQueenColumns(queenOne, queenTwo)) {
+						// If swapQueenColumns returns True, swap was valid and
+						// we check new cost and have to inverse the move
+						int currentCost = sol.getCost();
+						if (currentCost < lowestCost) {
+							lowestCost = currentCost;
+							bestSwap[0] = queenOne;
+							bestSwap[1] = queenTwo;
+						}
+
+						sol.undoLocalSwap(queenOne, queenTwo);
+					}
+				}
+			}
+
+			// If we have reached a local minimum, the best improvement is as good as the current solution and thus
+			// we never got into the if statement that modifies bestMove, so we quit the while loop
+			if (bestSwap[0] == -1)
+				break;
+
+			sol.swapQueenColumns(bestSwap[0], bestSwap[1]);
+		}
 	}
 	
 }
